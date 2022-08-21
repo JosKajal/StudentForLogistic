9be801d310207161faeb9e7dee666f6db7d74ba4
Jose Kajal <josekajal_reynosa@hotmail.com>
2022-08-20 21:08:18 -0700

    Agregar archivos de proyecto.

---
+++ StudentForLogistic/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using StudentForLogistic.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace StudentForLogistic.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
