00581f9126d133bfde9ae5193f1df1265653c081
Jose Kajal <josekajal_reynosa@hotmail.com>
2022-08-25 19:09:12 -0700

    Avance mapa

--- StudentForLogistic/AppShell.xaml.cs
+++ StudentForLogistic/AppShell.xaml.cs
@@ -11,7 +11,7 @@ namespace StudentForLogistic
         public AppShell()
         {
             InitializeComponent();
-           
+
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
